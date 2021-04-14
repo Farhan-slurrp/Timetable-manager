1e0db2c2acd172aa6bb8ce4dff57b523a735e846
Farhan Ahmad Nurzi <58872254+Farhan-slurrp@users.noreply.github.com>
2021-04-14 22:12:55 +0800

    update loginactivity

--- app/src/main/java/com/example/timetablemanager/LoginActivity.java
+++ app/src/main/java/com/example/timetablemanager/LoginActivity.java
@@ -93,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                         String[] data = new String[2];
                         data[0] = username;
                         data[1] = password;
-                        PutData putData = new PutData("http://192.168.43.33/Timetable/login.php", "POST", field, data);
+                        PutData putData = new PutData("http://localhost/Timetable/login.php", "POST", field, data);
                         if (putData.startPut()) {
                             if (putData.onComplete()) {
                                 String result = putData.getResult();
